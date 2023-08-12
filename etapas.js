bc4a718fea12f5a56d12b61d592f7389c51a4101
Pedro Lucas <pedrolucaas762@gmail.com>
2023-08-11 21:50:25 -0300

    Updated

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 22222,
+                nome: "Pedro Lucas",
+                partido: "PDL",
+                fotos: [
+                    { url: '38111.jpg', legenda: 'Vereador' }
+                ]
+            },
+            {
+                numero: 55555,
+                nome: "João Pedro",
+                partido: "JPD",
+                fotos: [
+                    { url: '77222.jpg', legenda: 'Vereador' }
+                ]
+            }
+        ]
+    },
+    {
+        titulo: "PREFEITO",
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 22,
+                nome: "José Silva",
+                partido: "PDL",
+                vice: "Cristina",
+                fotos: [
+                    { url: '99.jpg', legenda: 'Prefeito'},
+                    { url: '99_2.jpg', legenda: 'Vice-Prefeito'}
+                ]
+            },
+            {
+                numero: 55,
+                nome: "Henrique Costa",
+                partido: "JPD",
+                vice: "Luiz ",
+                fotos: [
+                    { url: '84.jpg', legenda: 'Prefeito'},
+                    { url: '84_2.jpg', legenda: 'Vice-Prefeito'}
+                ]
+            }
+        ]
+    }
+];
